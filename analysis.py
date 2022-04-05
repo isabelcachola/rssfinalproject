6259235de044c9a7489aa9c4e762a360b1ba3ce5
Elizabeth Salesky <elizabeth.salesky@gmail.com>
2022-04-04 21:42:15 -0400

    analysis to script

--- analysis.py
+++ analysis.py
@@ -7,11 +7,27 @@ import time
 import torch
 import transformers
 import itertools
+from collections import defaultdict
+
+from models import MTModel
+
+# Use with care: logging error only while printing analysis for reading sanity
+transformers.utils.logging.set_verbosity_error()
+
 
 def output_diff(alignment, translation):
     pass
 
-def align(source, target):
+def get_out_token(src_idx, s2t, output):
+    #get 1-best ali
+    out_idx = list(s2t[src_idx])[0]
+    #get token from idx
+    tmp = output.split()
+    out_token = tmp[out_idx]
+    return out_token
+
+# Align source and target word sequences with the awesome aligner (expects non-tokenized input) 
+def align(src, tgt):
     model = transformers.BertModel.from_pretrained('bert-base-multilingual-cased')
     tokenizer = transformers.BertTokenizer.from_pretrained('bert-base-multilingual-cased')
 
@@ -40,13 +56,17 @@ def align(source, target):
         softmax_srctgt = torch.nn.Softmax(dim=-1)(dot_prod)
         softmax_tgtsrc = torch.nn.Softmax(dim=-2)(dot_prod)
 
-        softmax_inter = (softmax_srctgt > threshold)*(softmax_tgtsrc > threshold)    
+        softmax_inter = (softmax_srctgt > threshold)*(softmax_tgtsrc > threshold)
+
+    # src2tgt is a dict mapping src words to their set of aligned tgt words; align_words is the set of alignments for printing alis etc
     align_subwords = torch.nonzero(softmax_inter, as_tuple=False)
     align_words = set()
+    src2tgt = defaultdict(set)
     for i, j in align_subwords:
-        align_words.add( (sub2word_map_src[i], sub2word_map_tgt[j]) )    
+        align_words.add( (sub2word_map_src[i], sub2word_map_tgt[j]) )
+        src2tgt[sub2word_map_src[i]].add(sub2word_map_tgt[j])
 
-    return align_words
+    return src2tgt, align_words
 
 def print_alignments(align_words):
     for i, j in sorted(align_words):
@@ -66,14 +86,50 @@ class color:
    UNDERLINE = '\033[4m'
    END = '\033[0m'
 
+#example: python analysis.py --lang_pair en-es --src "this is a test" --swap_idx 3 --swap_val sentence
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
-    # parser.add_argument('example')
+    parser.add_argument('--lang_pair')
+    parser.add_argument('--src')
+    parser.add_argument('--swap_idx', action='store', type=int)
+    parser.add_argument('--swap_val')
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(message)s')
 
+    # -- swap analysis -- 
     start = time.time()
-    # Do things here
+
+    #instantiate model
+    model    = MTModel(args.lang_pair)
+    src_idx  = args.swap_idx
+    
+    #standard setting
+    src     = args.src
+    out     = model.translation_from_string(src)
+    s2t, _  = align(src,out)
+
+    #noised source
+    src_word = src.split()[ src_idx ]
+    src_swap = args.swap_val
+    src_cos  = model.compute_cos(model.get_embed_from_text(src_word), model.get_embed_from_text(src_swap))
+    print("cossim between src (%s) and sub (%s) is: %f." % (src_word, src_swap, src_cos))
+
+    #do swap
+    tmp = src.split()
+    tmp[src_idx] = src_swap
+    swap_src = ' '.join(tmp)
+    swap_out = model.translation_from_string(swap_src)
+    swap_s2t, _  = align(swap_src,swap_out)
+    
+    #noised output
+    out_word = get_out_token(src_idx, s2t, out)
+    out_swap = get_out_token(src_idx, swap_s2t, swap_out)
+    out_cos  = model.compute_cos(model.get_embed_from_text(out_word), model.get_embed_from_text(out_swap))
+    print("cossim between output (%s) and sub (%s) is: %f." % (out_word, out_swap, out_cos))    
+
+    print(out)
+    print(swap_out)
+
     end = time.time()
     logging.info(f'Time to run script: {end-start} secs')
